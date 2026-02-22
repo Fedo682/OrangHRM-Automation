364be4c75c741a7fd3be223cc9ab44ca6095330a
mariam abulail <mariamabulail88@gmail.com>
2026-02-22 13:30:20 +0200

    Add TC33 test cases

--- src/main/java/AutomationPRJ/OrangHRM/AdminJobTitlesPage.java
+++ src/main/java/AutomationPRJ/OrangHRM/AdminJobTitlesPage.java
@@ -14,7 +14,7 @@ public class AdminJobTitlesPage {
 
 	public AdminJobTitlesPage(WebDriver driver) {
 		this.driver = driver;
-		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 	}
 
 	By adminTap = By.xpath("//span[normalize-space()='Admin']");
@@ -29,10 +29,12 @@ public class AdminJobTitlesPage {
 	By requiredMessage = By.xpath("//span[text()='Required']");
 	By cancelButton = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[5]/button[1]");
 	By editButton = By.xpath("//button[.//i[contains(@class,'bi-pencil-fill')]][1]");
-	By editJobTitle=By.xpath("//h6[text()='Edit Job Title']");
-	
-	By updateJobTitleFiled=By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input");
-	By saveEditButton=By.xpath("//button[normalize-space(.)='Save']");
+	By editJobTitle = By.xpath("//h6[text()='Edit Job Title']");
+
+	By updateJobTitleFiled = By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input");
+	By saveEditButton = By.xpath("//button[normalize-space(.)='Save']");
+	By cancelEditButton = By.xpath("//button[normalize-space(.)='Cancel']");
+
 	public String goToMainJobTitlePage() {
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
@@ -65,19 +67,20 @@ public class AdminJobTitlesPage {
 
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		longWait.until(ExpectedConditions.or(ExpectedConditions.visibilityOfElementLocated(jobTitleMainPage),
-				ExpectedConditions.visibilityOfElementLocated(alreadyExistsMessage),ExpectedConditions.visibilityOfElementLocated(requiredMessage)));
+				ExpectedConditions.visibilityOfElementLocated(alreadyExistsMessage),
+				ExpectedConditions.visibilityOfElementLocated(requiredMessage)));
 
 	}
+
 	public void clickCancel() {
 		wait.until(ExpectedConditions.elementToBeClickable(cancelButton)).click();
-
-		
 
 	}
 
 	public String existedJobTitle() {
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		return longWait.until(ExpectedConditions.visibilityOfElementLocated(firstJobTitle)).getText();	}
+		return longWait.until(ExpectedConditions.visibilityOfElementLocated(firstJobTitle)).getText();
+	}
 
 	public String getAlreadyExistsMessage() {
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(alreadyExistsMessage)).getText();
@@ -88,27 +91,23 @@ public class AdminJobTitlesPage {
 		return wait.until(ExpectedConditions.visibilityOfElementLocated(requiredMessage)).getText();
 
 	}
+
 	public void clickEditIcon() {
-	    By firstJobRow = By.xpath("(//div[@role='row'])[2]");
-	    wait.until(ExpectedConditions.visibilityOfElementLocated(firstJobRow));
+		By firstJobRow = By.xpath("(//div[@role='row'])[2]");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(firstJobRow));
 
-	   
-
-	    WebElement editBtn = wait.until(ExpectedConditions.elementToBeClickable(editButton));
-	    editBtn.click();
+		WebElement editBtn = wait.until(ExpectedConditions.elementToBeClickable(editButton));
+		editBtn.click();
 	}
-	public void editJobTitle(String editJob) {
 
+	public void editJobTitle(String editJob) {
 		WebElement input = wait.until(ExpectedConditions.elementToBeClickable(updateJobTitleFiled));
 		input.click(); // focus
 		input.clear();
-		input.click(); 
+		input.click();
 		input.sendKeys(editJob);
-		
-	
-
 	}
-	
+
 	public String goToEditJobTitlePage() {
 		WebDriverWait longWait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
@@ -117,6 +116,14 @@ public class AdminJobTitlesPage {
 
 	public void clcikSaveEditButton() {
 		wait.until(ExpectedConditions.elementToBeClickable(saveEditButton)).click();
+
+	}
+
+	public void clcikCancelEditButton() {
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+		wait.until(ExpectedConditions.elementToBeClickable(cancelEditButton)).click();
 
 	}
 
--- src/test/java/AutomationPRJ/OrangHRM/TestAdminJobTitlesPage.java
+++ src/test/java/AutomationPRJ/OrangHRM/TestAdminJobTitlesPage.java
@@ -2,8 +2,11 @@ package AutomationPRJ.OrangHRM;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -14,107 +17,117 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestAdminJobTitlesPage {
-    WebDriver driver;
-    Login loginPage;
-    AdminJobTitlesPage adminJobTitlesPage;
-    OrangHRM oranghrm;
+	WebDriver driver;
+	Login loginPage;
+	AdminJobTitlesPage adminJobTitlesPage;
+	OrangHRM oranghrm;
 
-    @BeforeTest
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        loginPage = new Login(driver);
-        adminJobTitlesPage = new AdminJobTitlesPage(driver);
-        oranghrm = new OrangHRM(driver);
-    }
+	@BeforeTest
+	public void setup() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		loginPage = new Login(driver);
+		adminJobTitlesPage = new AdminJobTitlesPage(driver);
+		oranghrm = new OrangHRM(driver);
+	}
 
-    @BeforeMethod
-    public void loginAndOpenJobTitles() {
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        loginPage.loginMethod("Admin", "admin123");
-        adminJobTitlesPage.jobTabThenTitleClick();
-    }
+	@BeforeMethod
+	public void loginAndOpenJobTitles() {
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		loginPage.loginMethod("Admin", "admin123");
+		adminJobTitlesPage.jobTabThenTitleClick();
+	}
 
-    @AfterMethod
-    public void logoutAfterTest() {
-        try {
-            oranghrm.logout();
-        } catch (Exception e) {
-            System.out.println("Not logged in or already logged out.");
-        }
-    }
+	@AfterMethod
+	public void logoutAfterTest() {
+		try {
+			oranghrm.logout();
+		} catch (Exception e) {
+			System.out.println("Not logged in or already logged out.");
+		}
+	}
 
-    @Test(priority = 1)
-    public void TC26_LoginAndOpenJobTitles() {
-        System.out.println("Login and open job title page");
-    }
+	@Test(priority = 1)
+	public void TC26_LoginAndOpenJobTitles() {
+		System.out.println("Login and open job title page");
+	}
 
-    @Test(priority = 2)
-    public void TC27_addNewJob() {
-        String newJob = "CSE_" + System.currentTimeMillis();
-        adminJobTitlesPage.clickAddButton();
-        adminJobTitlesPage.addNewJob(newJob);
-        adminJobTitlesPage.clickSubmit();
-        String expectedResult = "Job Titles";
-        Assert.assertEquals(adminJobTitlesPage.goToMainJobTitlePage(), expectedResult);
-        System.out.println("Job added successfully");
-    }
+	@Test(priority = 2)
+	public void TC27_addNewJob() {
+		String newJob = "CSE_" + System.currentTimeMillis();
+		adminJobTitlesPage.clickAddButton();
+		adminJobTitlesPage.addNewJob(newJob);
+		adminJobTitlesPage.clickSubmit();
+		String expectedResult = "Job Titles";
+		Assert.assertEquals(adminJobTitlesPage.goToMainJobTitlePage(), expectedResult);
+		System.out.println("Job added successfully");
+	}
 
-    @Test(priority = 3)
-    public void TC28_addExsistJob() {
-        String existedJobTitle = adminJobTitlesPage.existedJobTitle();
-        adminJobTitlesPage.clickAddButton();
-        adminJobTitlesPage.addNewJob(existedJobTitle);
-        adminJobTitlesPage.clickSubmit();
-        String expectedResult = "Already exists";
-        Assert.assertEquals(adminJobTitlesPage.getAlreadyExistsMessage(), expectedResult);
-        System.out.println("Duplicate jobs");
-    }
+	@Test(priority = 3)
+	public void TC28_addExsistJob() {
+		String existedJobTitle = adminJobTitlesPage.existedJobTitle();
+		adminJobTitlesPage.clickAddButton();
+		adminJobTitlesPage.addNewJob(existedJobTitle);
+		adminJobTitlesPage.clickSubmit();
+		String expectedResult = "Already exists";
+		Assert.assertEquals(adminJobTitlesPage.getAlreadyExistsMessage(), expectedResult);
+		System.out.println("Duplicate jobs");
+	}
 
-    @Test(priority = 4)
-    public void TC29_testAddJobWithEmptyJobTitle() {
-        adminJobTitlesPage.clickAddButton();
-        adminJobTitlesPage.addNewJob("");
-        adminJobTitlesPage.clickSubmit();
-        String expectedResult = "Required";
-        Assert.assertEquals(adminJobTitlesPage.getRequiredMessage(), expectedResult);
-        System.out.println("Jobs Title Required");
-    }
+	@Test(priority = 4)
+	public void TC29_testAddJobWithEmptyJobTitle() {
+		adminJobTitlesPage.clickAddButton();
+		adminJobTitlesPage.addNewJob("");
+		adminJobTitlesPage.clickSubmit();
+		String expectedResult = "Required";
+		Assert.assertEquals(adminJobTitlesPage.getRequiredMessage(), expectedResult);
+		System.out.println("Jobs Title Required");
+	}
 
-    @Test(priority = 5)
-    public void TC30_verifyAdminCanCancelAddingAJobTitle() {
-        adminJobTitlesPage.clickAddButton();
-        adminJobTitlesPage.addNewJob("QA");
-        adminJobTitlesPage.clickCancel();
-        String expectedResult = "Job Titles";
-        Assert.assertEquals(adminJobTitlesPage.goToMainJobTitlePage(), expectedResult);
-        System.out.println("Job canceled successfully");
-    }
+	@Test(priority = 5)
+	public void TC30_verifyAdminCanCancelAddingAJobTitle() {
+		adminJobTitlesPage.clickAddButton();
+		adminJobTitlesPage.addNewJob("QA");
+		adminJobTitlesPage.clickCancel();
+		String expectedResult = "Job Titles";
+		Assert.assertEquals(adminJobTitlesPage.goToMainJobTitlePage(), expectedResult);
+		System.out.println("Job canceled successfully");
+	}
 
-    @Test(priority = 6)
-    public void TC31_verifyTheEditIconNavigateToEditJobTitle() {
-    	
-        adminJobTitlesPage.clickEditIcon();
-        String expectedResult = "Edit Job Title";
-        Assert.assertEquals(adminJobTitlesPage.goToEditJobTitlePage(), expectedResult);
-        System.out.println("Navigate to Edit Job Title successfully");
-    }
- 
-    @Test(priority = 7)
-    public void TC32_verifyToEditJobTitle() {
-        adminJobTitlesPage.clickEditIcon();
-        adminJobTitlesPage.editJobTitle("new");
-        adminJobTitlesPage.clcikSaveEditButton();
-        String expectedResult = "Edit Job Title";
-        Assert.assertEquals(adminJobTitlesPage.goToEditJobTitlePage(), expectedResult);
-        System.out.println("Edit Job Title successfully");
-    }
+	@Test(priority = 6)
+	public void TC31_verifyTheEditIconNavigateToEditJobTitle() {
 
-    @AfterTest
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		adminJobTitlesPage.clickEditIcon();
+		String expectedResult = "Edit Job Title";
+		Assert.assertEquals(adminJobTitlesPage.goToEditJobTitlePage(), expectedResult);
+		System.out.println("Navigate to Edit Job Title successfully");
+	}
+
+	@Test(priority = 7)
+	public void TC32_verifyToEditJobTitle() {
+		adminJobTitlesPage.clickEditIcon();
+		adminJobTitlesPage.editJobTitle("new");
+		adminJobTitlesPage.clcikSaveEditButton();
+		String expectedResult = "Edit Job Title";
+		Assert.assertEquals(adminJobTitlesPage.goToEditJobTitlePage(), expectedResult);
+		System.out.println("Edit Job Title successfully");
+	}
+
+	@Test(priority = 8)
+	public void TC33_verifyAdmincanCancelEditingAJobTitle() {
+		adminJobTitlesPage.clickEditIcon();
+		adminJobTitlesPage.editJobTitle("new");
+		adminJobTitlesPage.clcikCancelEditButton();
+		String expectedResult = "Job Titles";
+		Assert.assertEquals(adminJobTitlesPage.goToMainJobTitlePage(), expectedResult);
+		System.out.println("Cancel editing the Job Title successfully");
+	}
+
+	@AfterTest
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
